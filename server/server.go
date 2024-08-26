5e257377c76bf3dff9f929d53067a87499582f88
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-26 13:12:31 +0600

    basic proxy on auth

--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 type Server struct {
@@ -18,10 +21,26 @@ func NewServer(port string) *Server {
 }
 
 func (s *Server) Run() {
+	s.Router.HandleFunc("GET /", s.ProxyAuth())
 	log.Printf("server is running on port %v \n", s.Port)
 	err := http.ListenAndServe(s.Port, s.Router)
 
 	if err != nil {
 		log.Fatal("server crashed")
 	}
+}
+
+func (s *Server) ProxyAuth() http.HandlerFunc {
+	fmt.Println("ok")
+	authUrl, _ := url.Parse("http://localhost:8082/")
+	proxy := &httputil.ReverseProxy{
+		Rewrite: func(pr *httputil.ProxyRequest) {
+			pr.SetURL(authUrl)
+			pr.Out.Host = pr.In.Host
+		},
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
 }
